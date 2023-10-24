2d143030470f386aa20f09b6bbd9bdd3f1446c22
hlbrand <hlbrand2995@gmail.com>
2023-10-24 08:35:56 +0700

    12 - Product Card

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
 import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
@@ -11,8 +12,8 @@ export default function Home() {
           <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) => {
-            return <div>{truncateText(product.name)}</div>;
+          {products.map((product: any, index) => {
+            return <ProductCard data={product} key={index} />;
           })}
         </div>
       </Container>
